d94df43b8482770d752f9c98c7e49beb3f403ed7
Ramakrishna551 <nadullaramakrishna@gmail.com>
2025-02-28 21:29:30 +0530

    samll edit done other features added

--- InheritenceExample/Threads/Threadpoolexecutor.py
+++ InheritenceExample/Threads/Threadpoolexecutor.py
@@ -8,3 +8,11 @@ def thread_funtion(name):
     print("Thread {0} ended ".format(name))
 with ThreadPoolExecutor(max_workers=3) as executor:
     executor.map(thread_funtion,range(3))
+    # executor.map(thread_funtion,range(3))`
+    # uses submit method also 
+    #i.e 
+    executor.submit(thread_funtion,(0,))
+    executor.submit(thread_funtion,(1,))
+    executor.submit(thread_funtion,(2,))
+    executor.submit(thread_funtion,(3,))
+    executor.submit(thread_funtion,(4,))
